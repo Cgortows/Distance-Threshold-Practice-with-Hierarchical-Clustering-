8c696b37d8a2395cfb4e2a9563ab900bcfe263ac
christopher gortowski <120144967+Cgortows@users.noreply.github.com>
2023-08-31 19:29:45 -0500

    Add files via upload

---
+++ Project_scrpit.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
+from sklearn.datasets import make_classification
+
+# Set a seed for reproducibility
+np.random.seed(42)
+
+# Create a synthetic dataset
+X, _ = make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=2, n_clusters_per_class=2, n_classes=2)
+
+# Convert to dataframe
+df_complex = pd.DataFrame(X, columns=[f'Feature_{i+1}' for i in range(X.shape[1])])
+
+# Initialize PCA with 2 components, Fit, Transform, and convert to a DataFrame
+pca_complex = PCA(n_components=2)
+pca_transformed_complex = pca_complex.fit_transform(df_complex)
+df_pca_complex = pd.DataFrame(pca_transformed_complex, columns=['PC1', 'PC2'])
+
+# Perform hierarchical clustering on PCA transformed data
+Z_pca_complex = linkage(df_pca_complex, method='ward')
+
+# Using a distance threshold to define the clusters
+distance_threshold = 100
+clusters = fcluster(Z_pca_complex, distance_threshold, criterion='distance')
+
+# Create a dendrogram
+# plt.figure(figsize=(10, 5))
+# plt.title('Dendogram')
+# plt.xlabel('Sample index')
+# plt.ylabel('Distance')
+# dendrogram(Z_pca_complex, leaf_rotation=90, leaf_font_size=8, color_threshold=100)
+# plt.show()
+
+# Scatter plot of the hierarchical clustering results with discrete colormap
+plt.figure(figsize=(8, 6))
+plt.scatter(df_pca_complex['PC1'], df_pca_complex['PC2'], c=clusters, cmap='tab10', s=30, alpha=0.5)
+plt.title('Five Informative Five Redundant')
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar(label='Cluster')
+plt.grid(True)
+plt.show()
+
+
+
